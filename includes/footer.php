508d6cab7488ee3564ca5d113bda3be0815141d5
GielinorR-S <jaydencini@gmail.com>
2025-09-16 21:48:35 +1000

    Add newsletter form backend handling and messages

--- includes/footer.php
+++ includes/footer.php
@@ -23,10 +23,30 @@
                     <a href="#" aria-label="Instagram"><span class="icon-instagram"></span></a>
                     <a href="#" aria-label="Twitter"><span class="icon-twitter"></span></a>
                 </div>
-                <form class="newsletter-form" action="/newsletter_subscribe.php" method="POST">
-                    <input type="email" name="email" placeholder="Your email address" required>
-                    <button type="submit">Subscribe</button>
-                </form>
+                <?php
+                $newsletter_success = false;
+                $newsletter_error = '';
+                if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'])) {
+                    $newsletter_email = $_POST['email'] ?? '';
+                    if ($newsletter_email && filter_var($newsletter_email, FILTER_VALIDATE_EMAIL)) {
+                        // Here you would normally save to a database or send an email
+                        $newsletter_success = true;
+                    } else {
+                        $newsletter_error = 'Please enter a valid email address.';
+                    }
+                }
+                ?>
+                <?php if ($newsletter_success): ?>
+                    <div class="success-message">Thank you for subscribing!</div>
+                <?php else: ?>
+                    <?php if ($newsletter_error): ?>
+                        <div class="error-message"><?php echo htmlspecialchars($newsletter_error); ?></div>
+                    <?php endif; ?>
+                    <form class="newsletter-form" action="" method="POST">
+                        <input type="email" name="email" placeholder="Your email address" required>
+                        <button type="submit">Subscribe</button>
+                    </form>
+                <?php endif; ?>
             </div>
         </div>
         <div class="footer-bottom">
